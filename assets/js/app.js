144d3b81f23e6c988743f609993fd9d874384833
Khaoula <55798632+AlKhaoula02@users.noreply.github.com>
2020-01-30 16:25:16 +0100

    maj

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,60 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore! saloute');
+console.log('Salut');
+
+var map = L.map('map').setView([46, 2], 7);
+// // création du calque images
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+// // ajout d'un markeur
+L.marker([46, 2]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.');
+    // .openPopup();
+    
+    fetch('map.php', {
+        method: 'GET', // or 'PUT'
+        cors: 'no-cors',
+        headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+        }
+        })
+        .then((responseFesti) => {
+        return responseFesti.json();
+        })
+        .then((myJsonFesti) => {
+        let myNewJsonFesti = JSON.parse(myJsonFesti);
+        
+        });
+
+
+    // fetch('/home', {
+    //     method: 'GET', // or 'PUT'
+    //     headers: {
+    //     'Content-Type': 'application/json',
+    //     'Access-Control-Allow-Origin': '*'
+    //     }
+    //     })
+    //     .then((responseFesti) => {
+    //     return responseFesti.json();
+    //     })
+    //     .then((myJsonFesti) => {
+    //     let myNewJsonFesti = JSON.parse(myJsonFesti);
+    //         console.log(myNewJsonFesti);
+            
+    //     // let baseFesti = myNewJsonFesti['records'];
+            
+    //     //     for (let index = 0; index < baseFesti.length; index++) {
+    //     //     const element = baseFesti[index]['fields'];
+            
+    //     //     let position = element["geo_point"]
+    //     //}
+    //     });
+
--- src/Controller/CurlController.php
+++ src/Controller/CurlController.php
@@ -12,8 +12,32 @@ class CurlController extends AbstractController
      */
     public function index()
     {
-        return $this->render('curl/index.html.twig', [
-            'controller_name' => 'CurlController',
-        ]);
+
+
+       
+
+        // $curl = curl_init();
+        
+        // curl_setopt_array($curl, array(
+        // CURLOPT_URL => "https://opendata.lillemetropole.fr/explore/dataset/panorama-des-festivals-metropolitains/download?format=json&amp",
+        // CURLOPT_RETURNTRANSFER => true,
+        // CURLOPT_TIMEOUT => 30,
+        // CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        // CURLOPT_CUSTOMREQUEST => "GET",
+        // CURLOPT_HTTPHEADER => array(
+        // "cache-control: no-cache"
+        // ),
+        // ));
+        
+        // $response = curl_exec($curl);
+        // $err = curl_error($curl);
+        
+        // curl_close($curl);
+        
+        
+        // return $this->render('curl/index.html.twig', [
+        //     'controller_name' => 'CurlController',
+        //     'json' => $response
+        // ]);
     }
 }
