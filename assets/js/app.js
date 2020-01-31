e303d3a37a523f417fcb0f620db6ecdb9d8376cd
alexformationofp <55798552+alexformationofp@users.noreply.github.com>
2020-01-31 15:46:43 +0100

    ca marche

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,59 +7,52 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 
-
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Salut');
+console.log("Salut");
 
-var map = L.map('map').setView([46, 2], 7);
+var map = L.map("map").setView([46, 2], 7);
 // // création du calque images
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 // // ajout d'un markeur
-L.marker([46, 2]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.');
-    // .openPopup();
+L.marker([46, 2])
+  .addTo(map)
+  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.");
+// .openPopup();
+
+
+
+fetch("/curl", {
+  method: "GET", // or 'PUT'
+  // cors: 'no-cors',
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*"
+  }
+})
+  .then(responseFesti => {
+    return responseFesti.json();
+  })
+  .then(function(json) {
+    console.log (json[0].fields.departement);
     
-    fetch('map.php', {
-        method: 'GET', // or 'PUT'
-        cors: 'no-cors',
-        headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-        }
-        })
-        .then((responseFesti) => {
-        return responseFesti.json();
-        })
-        .then((myJsonFesti) => {
-        let myNewJsonFesti = JSON.parse(myJsonFesti);
+
+    for (let i = 0; i < json.length; i++) {
+        lat = json[i].fields.coordonnees_insee[0];
+        lon = json[i].fields.coordonnees_insee[1];
+        nom = json[i].fields.nom_de_la_manifestation;
+        console.log(lat,lon);   
+        L.marker([lat, lon])
+        .addTo(map)
+         .bindPopup(nom); 
         
-        });
+    }
+});
 
 
-    // fetch('/home', {
-    //     method: 'GET', // or 'PUT'
-    //     headers: {
-    //     'Content-Type': 'application/json',
-    //     'Access-Control-Allow-Origin': '*'
-    //     }
-    //     })
-    //     .then((responseFesti) => {
-    //     return responseFesti.json();
-    //     })
-    //     .then((myJsonFesti) => {
-    //     let myNewJsonFesti = JSON.parse(myJsonFesti);
-    //         console.log(myNewJsonFesti);
-            
-    //     // let baseFesti = myNewJsonFesti['records'];
-            
-    //     //     for (let index = 0; index < baseFesti.length; index++) {
-    //     //     const element = baseFesti[index]['fields'];
-            
-    //     //     let position = element["geo_point"]
-    //     //}
-    //     });
 
+  
--- src/Controller/CurlController.php
+++ src/Controller/CurlController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Response;
 class CurlController extends AbstractController
 {
     /**
@@ -12,32 +12,25 @@ class CurlController extends AbstractController
      */
     public function index()
     {
-
-
-       
-
-        // $curl = curl_init();
+        $curl = curl_init();
         
-        // curl_setopt_array($curl, array(
-        // CURLOPT_URL => "https://opendata.lillemetropole.fr/explore/dataset/panorama-des-festivals-metropolitains/download?format=json&amp",
-        // CURLOPT_RETURNTRANSFER => true,
-        // CURLOPT_TIMEOUT => 30,
-        // CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        // CURLOPT_CUSTOMREQUEST => "GET",
-        // CURLOPT_HTTPHEADER => array(
-        // "cache-control: no-cache"
-        // ),
-        // ));
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://data.culture.gouv.fr/explore/dataset/panorama-des-festivals/download?format=json&timezone=Europe/Berlin&use_labels_for_header=true",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+        "cache-control: no-cache",
+        "Access-Control-Allow-Origin: '*'"
+        ),
+        ));
         
-        // $response = curl_exec($curl);
-        // $err = curl_error($curl);
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
         
-        // curl_close($curl);
+        curl_close($curl);
         
-        
-        // return $this->render('curl/index.html.twig', [
-        //     'controller_name' => 'CurlController',
-        //     'json' => $response
-        // ]);
+        return new Response($response);
     }
 }
