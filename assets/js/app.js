17077b2e89946c07d70a92c80dbc1aa080b9b700
alexformationofp <55798552+alexformationofp@users.noreply.github.com>
2020-02-03 08:48:06 +0100

    essais menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,27 +4,22 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
 // any CSS you import will output into a single css file (app.css in this case)
-
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
-
 console.log("Salut");
 
-var map = L.map("map").setView([46, 2], 7);
+var map = L.map("map").setView([46, 2], 6);
 // // création du calque images
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+ 
+    maxZoom :18
 }).addTo(map);
 // // ajout d'un markeur
 L.marker([46, 2])
   .addTo(map)
   .bindPopup("A pretty CSS3 popup.<br> Easily customizable.");
 // .openPopup();
-
-
 
 fetch("/curl", {
   method: "GET", // or 'PUT'
@@ -38,19 +33,20 @@ fetch("/curl", {
     return responseFesti.json();
   })
   .then(function(json) {
-    console.log (json[0].fields.departement);
-    
+    console.log (json[0].fields.departement);  
 
     for (let i = 0; i < json.length; i++) {
+
+      if (json[i].fields.domaine=="Musiques actuelles"){ 
         lat = json[i].fields.coordonnees_insee[0];
         lon = json[i].fields.coordonnees_insee[1];
         nom = json[i].fields.nom_de_la_manifestation;
-        console.log(lat,lon);   
+        // console.log(lat,lon);   
         L.marker([lat, lon])
         .addTo(map)
          .bindPopup(nom); 
-        
-    }
+        console.log(json[i].fields.domaine.length);
+    }}
 });
 
 
