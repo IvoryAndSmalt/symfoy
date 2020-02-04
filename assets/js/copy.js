65f2dd0ca4e7a6ae883a31ca0b1be5170a837491
alexformationofp <55798552+alexformationofp@users.noreply.github.com>
2020-02-04 16:56:09 +0100

    fin des fonctionnalités

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -7,11 +7,11 @@
 // any CSS you import will output into a single css file (app.css in this case)
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+console.log("Salut");
 let selectDomaines = document.querySelector("#domaine");
 let selectDepartements = document.querySelector("#departement");
 let selectMois = document.querySelector("#mois");
 let markers = [];
-const message = document.querySelector('#message');
 
 // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx AFFICHAGE CARTE XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
@@ -38,35 +38,33 @@ fetch("/curl", {
   })
   .then(function(json) {
     // console.log (json[0].fields.departement);
-    function creerMarker() {
-      newMarker = L.marker([lat, lon])
-        .addTo(map)
-        .bindPopup(
-          "<strong>" + nom + "</strong>" + "<br/>" + domaine +"<br/>"+ commune +lien
-        );
-      markers.push(newMarker);
-    }
+
     for (let i = 0; i < json.length; i++) {
       if (
         json[i].geometry != undefined &&
         json[i].fields.nom_de_la_manifestation != undefined &&
         json[i].fields.domaine != undefined &&
-        json[i].fields.commune_principale != undefined &&
         json[i].fields.mois_habituel_de_debut != undefined
       ) {
         lat = json[i].geometry.coordinates[1];
-        lon = json[i].geometry.coordinates[0]; 
-        lien="<p>N'a pas de site</p>";
-        if (json[i].fields.site_web !== undefined) {
-          lien = "<br/><a target='_blank' href='"+ json[i].fields.site_web +"'>"+json[i].fields.site_web+"</a>"
-        }     
+        lon = json[i].geometry.coordinates[0];
+
         nom = json[i].fields.nom_de_la_manifestation;
         domaine = json[i].fields.domaine;
         mois = json[i].fields.mois_habituel_de_debut;
-       commune = json[i].fields.commune_principale;
+        function creerMarker (){
+             newMarker = L.marker([lat, lon])
+          .addTo(map)
+          .bindPopup(
+            "<strong>" + nom + "</strong>" + "<br/>" + domaine + "<br/>" + mois
+          );
+        markers.push(newMarker);
+        }
         creerMarker();
+     
       }
     }
+    console.log(markers);
 
     function onlyUnique(value, index, self) {
       return self.indexOf(value) === index;
@@ -120,10 +118,7 @@ fetch("/curl", {
     const formulaire = document.querySelector("#formulaire");
 
     function cacherMarqueurs() {
-      for (i = 0; i < markers.length; i++) {
-        map.removeLayer(markers[i]);
-      }
-      markers = [];
+      for (i = 0; i < markers.length; i++) map.removeLayer(markers[i]);
     }
 
     formulaire.addEventListener("submit", function(e) {
@@ -144,80 +139,177 @@ fetch("/curl", {
           domaine = json[i].fields.domaine;
           commune = json[i].fields.commune_principale;
           mois = json[i].fields.mois_habituel_de_debut;
-          lien="<p>N'a pas de site</p>";
-          if (json[i].fields.site_web !== undefined) {
-            lien = "<br/><a target='_blank' href='"+ json[i].fields.site_web +"'>"+json[i].fields.site_web+"</a>"
-          } 
-     
           if (
             domaine == selectDomaines.value &&
             mois == selectMois.value &&
             selectDepartements.value == "Tous"
           ) {
-            creerMarker();
-            map.flyTo([46, 2], 6);
+            newMarker = L.marker([lat, lon])
+              .addTo(map)
+              .bindPopup(
+                "<strong>" +
+                  nom +
+                  "</strong>" +
+                  "<br/>" +
+                  domaine +
+                  "<br/>" +
+                  commune +
+                  "<br/>" +
+                  mois
+              );
+            markers.push(newMarker);
           } else if (
             departement == selectDepartements.value &&
             mois == selectMois.value &&
             selectDomaines.value == "Tous"
           ) {
-            creerMarker();
-            map.flyTo([lat, lon], 8);
+            newMarker = L.marker([lat, lon])
+              .addTo(map)
+              .bindPopup(
+                "<strong>" +
+                  nom +
+                  "</strong>" +
+                  "<br/>" +
+                  domaine +
+                  "<br/>" +
+                  commune +
+                  "<br/>" +
+                  mois
+              );
+            markers.push(newMarker);
           } else if (
             departement == selectDepartements.value &&
             domaine == selectDomaines.value &&
             selectMois.value == "Tous"
           ) {
-            creerMarker();
-            map.flyTo([lat, lon], 8);
+            newMarker = L.marker([lat, lon])
+              .addTo(map)
+              .bindPopup(
+                "<strong>" +
+                  nom +
+                  "</strong>" +
+                  "<br/>" +
+                  domaine +
+                  "<br/>" +
+                  commune +
+                  "<br/>" +
+                  mois
+              );
+            markers.push(newMarker);
           } else if (
             departement == selectDepartements.value &&
             domaine == selectDomaines.value &&
             mois == selectMois.value
           ) {
-            creerMarker();
-            map.flyTo([lat, lon], 8);
+            newMarker = L.marker([lat, lon])
+              .addTo(map)
+              .bindPopup(
+                "<strong>" +
+                  nom +
+                  "</strong>" +
+                  "<br/>" +
+                  domaine +
+                  "<br/>" +
+                  commune +
+                  "<br/>" +
+                  mois
+              );
+            markers.push(newMarker);
           } else if (
             departement == selectDepartements.value &&
             selectDomaines.value == "Tous" &&
             selectMois.value == "Tous"
           ) {
-            creerMarker();
-            map.flyTo([lat, lon], 8);
+            newMarker = L.marker([lat, lon])
+              .addTo(map)
+              .bindPopup(
+                "<strong>" +
+                  nom +
+                  "</strong>" +
+                  "<br/>" +
+                  domaine +
+                  "<br/>" +
+                  commune +
+                  "<br/>" +
+                  mois
+              );
+            markers.push(newMarker);
           } else if (
-            "Tous" == selectDepartements.value &&
+            'Tous' == selectDepartements.value &&
             selectDomaines.value == domaine &&
             selectMois.value == "Tous"
           ) {
-            creerMarker();
-            map.flyTo([46, 2], 6);
-          } else if (
-            "Tous" == selectDepartements.value &&
+            newMarker = L.marker([lat, lon])
+              .addTo(map)
+              .bindPopup(
+                "<strong>" +
+                  nom +
+                  "</strong>" +
+                  "<br/>" +
+                  domaine +
+                  "<br/>" +
+                  commune +
+                  "<br/>" +
+                  mois
+              );
+            markers.push(newMarker);
+          }else if (
+            'Tous' == selectDepartements.value &&
             selectDomaines.value == "Tous" &&
             selectMois.value == mois
           ) {
-            creerMarker();
-            map.flyTo([46, 2], 6);
-          } else if (
-            "Tous" == selectDepartements.value &&
+            newMarker = L.marker([lat, lon])
+              .addTo(map)
+              .bindPopup(
+                "<strong>" +
+                  nom +
+                  "</strong>" +
+                  "<br/>" +
+                  domaine +
+                  "<br/>" +
+                  commune +
+                  "<br/>" +
+                  mois
+              );
+            markers.push(newMarker);
+          }else if (
+            'Tous' == selectDepartements.value &&
             selectDomaines.value == "Tous" &&
             selectMois.value == "Tous"
           ) {
-            creerMarker();
-            map.flyTo([46, 2], 6);
+            newMarker = L.marker([lat, lon])
+              .addTo(map)
+              .bindPopup(
+                "<strong>" +
+                  nom +
+                  "</strong>" +
+                  "<br/>" +
+                  domaine +
+                  "<br/>" +
+                  commune +
+                  "<br/>" +
+                  mois
+              );
+            markers.push(newMarker);
           }
         }
       }
-      if (markers.length == 0) {
-          message.innerHTML = 'Aucun résultat ne correspond à votre recherche !';
-      }else if (markers.length ==1){
-        message.innerHTML = markers.length + ' résultat correspond à votre recherche';
-      }else if (markers.length >1){
-        message.innerHTML = markers.length + ' résultats correspondent à votre recherche';
-      }
-      //  TESTER ICI LA LONGUEUR DE MARKERS
-      //  ICI ON PEUT DIRE A L'UTILISATEUR QU'IL N4Y A AUCUN RESULTAT
-      console.log(markers.length);
+      //  cacherMarqueurs();
     });
-    document.querySelector('#loader').classList.add('cache');
   });
+// departement == selectDepartements.value  && domaine == selectDomaines.value && mois == selectMois.value) {
+
+//                 newMarker = L.marker([lat, lon])
+//                   .addTo(map)
+//                   .bindPopup(
+//                     "<strong>" +
+//                       nom +
+//                       "</strong>" +
+//                       "<br/>" +
+//                       domaine +
+//                       "<br/>" +
+//                       commune +
+//                       "<br/>" +
+//                       mois
+//                   );
+//                 markers.push(newMarker);
